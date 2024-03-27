7b8d31a7621e87c6b7d416587d4045d16251d7ad
anilyavas <anil.yavas99@gmail.com>
2024-03-27 18:44:54 +0300

    render posts

---
+++ src/components/PostListItem.tsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const PostListItem = ({ post }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{post.title}</Text>
+      <Text style={styles.body}>{post.body}</Text>
+    </View>
+  );
+};
+
+export default PostListItem;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    padding: 10,
+    gap: 10,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  body: {
+    color: 'grey',
+  },
+});
