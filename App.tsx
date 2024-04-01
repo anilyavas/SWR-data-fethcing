6142cf510924b27736515b7446b50972ae266d73
anilyavas <anil.yavas99@gmail.com>
2024-04-01 21:26:05 +0300

    Revalidate on app focus and on network reconnet

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, AppState } from 'react-native';
 import PostsScreen from './src/app/PostsScreen';
 import GraphQLPostScreen from './src/app/GraphQLPostScreen';
 import { SWRConfig } from 'swr';
 import { fetcher } from './src/utils/fetcher';
+import { SWRConfiguration } from './src/utils/SWRConfiguration';
 
 export default function App() {
   return (
-    <SWRConfig value={{ fetcher, dedupingInterval: 2000 }}>
+    <SWRConfig value={SWRConfiguration}>
       <SafeAreaView style={styles.container}>
-        <GraphQLPostScreen />
+        <PostsScreen />
         <StatusBar style='auto' />
       </SafeAreaView>
     </SWRConfig>
